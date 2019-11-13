d89362aa48747e8a6f173047aff8ff2c7fe520a8
Amit Jakubowicz <jakubowicz.amit@gmail.com>
2019-11-13 07:09:27 +0100

    Add select

--- packages/qpa/Event/Recurrence/RecurrencePicker.tsx
+++ packages/qpa/Event/Recurrence/RecurrencePicker.tsx
@@ -1,15 +1,20 @@
-import * as React from 'react'
-import rrule from 'rrule'
+import * as React from "react"
+import rrule from "rrule"
+import styled from "@emotion/styled"
 
 interface Props {
-    firstOccurrence: {
-        startDate: string
-        endDate: string
-    }
-    rrule: string
-    onChange: (rrule: string) => void
+  firstOccurrence: {
+    startDate: string
+    endDate: string
+  }
+  rrule: string
+  onChange: (rrule: string) => void
 }
 
 const RecurrencePicker = (props: Props) => {
+  return <Root>
 
+  </Root>
 }
+
+const Root = styled.div``
