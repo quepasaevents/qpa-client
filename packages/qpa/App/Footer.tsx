e5ba8ad19527a56b1767ac6a1864008d50228fd4
Amit Jakubowicz <jakubowicz.amit@gmail.com>
2019-12-10 11:10:35 +0100

    Internationalize footer message

--- packages/qpa/App/Footer.tsx
+++ packages/qpa/App/Footer.tsx
@@ -1,20 +1,23 @@
 import * as React from "react"
 import styled, { useTheme } from "qpa-emotion"
-
+import messages from "./Footer.msg.json"
+import intl from "react-intl-universal"
 interface Props {
   className?: string
 }
 const Footer = (props: Props) => {
   const theme = useTheme()
+  intl.load(messages)
   return (
     <Root
       className={props.className}
       css={{
         background: theme.colors.secondary,
-          textAlign: 'center'
-
+        textAlign: "center",
       }}
-    >Hecho con &#9829; en la Alpujarra</Root>
+    >
+      {intl.get("footer-message")}
+    </Root>
   )
 }
 const Root = styled.div``
